9e4de32befe97a98f2dcd2713f9244e677f5bb99
Juan Aramburo <2020030187@upsin.edu.mx>
2025-06-06 01:21:17 -0700

    Creacion del proyecto

---
+++ app/src/main/java/com/example/preexamen/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.preexamen
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var txtEmpleado : EditText
+    private lateinit var btnEntrar : Button
+    private lateinit var btnSalir : Button
+
+    fun IniciarComponentes(){
+        txtEmpleado = findViewById(R.id.txtEmpleado) as EditText
+        btnEntrar = findViewById(R.id.btnEntrar) as Button
+        btnSalir = findViewById(R.id.btnSalir) as Button
+    }
+
+    fun eventosBotones(){
+        btnEntrar.setOnClickListener(View.OnClickListener {
+            if (txtEmpleado.text.toString().isEmpty()){
+                Toast.makeText(this,"Falto capturar el nombre",Toast.LENGTH_SHORT).show()
+            } else{
+                val intent = Intent(this,ReciboNominaActivity::class.java)
+                intent.putExtra("Cliente",txtEmpleado.text.toString())
+                startActivity(intent)
+            }
+        })
+        btnSalir.setOnClickListener(View.OnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Nomina")
+            builder.setMessage("¿Deseas salir de la aplicacion")
+
+            builder.setPositiveButton("Aceptar"){dialog, which ->
+                finish()
+            }
+            builder.setNegativeButton("Cancelar") { dialog, which ->
+                Toast.makeText(
+                    applicationContext,
+                    "Quiza", Toast.LENGTH_SHORT).show()
+            }
+            builder.show()
+        })
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/preexamen/ReciboNomina.kt
@@ -0,0 +1,49 @@
+package com.example.preexamen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.io.Serializable
+import kotlin.random.Random
+
+class ReciboNomina : Serializable {
+
+    var numRecibo :  Int = 0
+    var Nombre : String = ""
+    var horasTrabNormal: Float = 0.0f
+    var horasTrabExtras: Float = 0.0f
+    var puesto: Int = 0
+    var pago: Int = 200
+    var impuestoPorc: Float = 16.0f  // 16% de impuesto
+
+    constructor(){
+        this.numRecibo = 0
+        this.Nombre = ""
+        this.horasTrabNormal = 0.0f
+        this.horasTrabExtras = 0.0f
+        this.puesto = 0
+        this.pago = 200
+        this.impuestoPorc = 16.0f
+    }
+
+    fun generarRecibo(): Int {
+        return Random.nextInt(0,5000)
+    }
+
+    fun CalcularSubtotal(): Float {
+        return (pago * horasTrabNormal) + ((pago * 2) * horasTrabExtras)
+    }
+
+    fun CalcularImpuestos(): Float {
+        val subtotal = CalcularSubtotal()
+        return (subtotal * impuestoPorc) / 100
+    }
+
+    fun CalcularTotal(): Float {
+        val subtotal = CalcularSubtotal()
+        val impuesto = CalcularImpuestos()
+        return subtotal - impuesto
+    }
+}
